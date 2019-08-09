c6efecab29fb6eb93333b1919cdb886ac4d7bcd6
emahiro <hiromichi.a.ema@dena.com>
2019-08-09 19:45:29 +0900

    add middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/emahiro/appengine-plain-logger/spancontext"
+)
+
+// MwAEPlainLogger is middleware for setting stackdrvier logging
+func MwAEPlainLogger(label string) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if label == "" {
+				panic("label is required")
+			}
+
+			ctx, done := spancontext.Set(r, label)
+			defer done()
+			h.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
