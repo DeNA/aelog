3c9253e2e127eef3de0fd4701703a975ce6cb2cf
emahiro <hiromichi.a.ema@dena.com>
2019-08-15 12:28:18 +0900

    fixup! add godoc of MwAEPlainLogger. write panic case is exist.

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,9 +6,9 @@ import (
 	"github.com/emahiro/ae-plain-logger/spancontext"
 )
 
-// MwAEPlainLogger is middleware for setting stackdrvier logging.
-// In this log middleware, label is required.
-// If you don't set label, this log middleware handle panic.
+// MwAEPlainLogger is middleware to set spancontext in the context.
+// In this middleware, label is required.
+// If you don't set label, this middleware panics.
 func MwAEPlainLogger(label string) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
